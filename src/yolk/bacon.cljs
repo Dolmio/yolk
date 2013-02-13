e604d23fe85d95236a4dd6e00de94a4170148090
Wilkes Joiner <wilkesjoiner@gmail.com>
2013-02-13 17:23:02 -0600

    reorg'd and added some creation functions

--- src/yolk/bacon.cljs
+++ src/yolk/bacon.cljs
@@ -1,33 +1,12 @@
 (ns yolk.bacon
   (:refer-clojure :exclude [map next filter merge]))
 
-(defn initial [x]
-  (js/Bacon.Initial. x))
-
-(defn next [x]
-  (js/Bacon.Next. x))
-
-(defn error [x]
-  (js/Bacon.Error. x))
-
-(defn end []
-  (js/Bacon.End.))
-
-(defn no-more? [reply]
-  (= (.-noMore js/Bacon) reply))
-
-
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Creation
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(defn event-stream [f]
-  (js/Bacon.EventStream. f))
-
-(defn from-event-target [target event-name]
-  (js/Bacon.fromEventTarget target event-name))
-
-(defn as-event-stream [f])
+(defn from-promise [promise]
+  (js/Bacon.fromPromise promise))
 
 (defn once
   ([]
@@ -35,19 +14,45 @@
   ([x]
      (js/Bacon.once x)))
 
+(defn from-array [values]
+  (js/Bacon.fromArray (into-array values)))
+
+(defn interval [ms value]
+  (js/Bacon.interval ms value))
+
+(defn sequentially [ms values]
+  (js/Bacon.sequentially ms values))
+
+(defn repeatedly [ms values]
+  (js/Bacon.repeatedly ms values))
+
+(defn never []
+  (js/Bacon.never))
+
+(defn from-event-target [target event-name]
+  (js/Bacon.fromEventTarget target event-name))
+
+(defn from-poll [interval f]
+  (js/Bacon.fromPoll interval f))
+
+(defn later [delay value]
+  (js/Bacon.later delay value))
+
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Observables
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defn map [stream f-or-property]
+  (.map stream (if (keyword? f-or-property)
+                 #(f-or-property %)
+                 f-or-property)))
 
 (defn delay [stream ms]
   (.delay stream ms))
 
 (defn filter [stream f]
   (.filter stream f))
-
-(defn map [stream f]
-  (.map stream (if (keyword? f) #(f %) f)))
 
 (defn on-value [stream f]
   (.onValue stream f))
@@ -64,9 +69,14 @@
 (defn do-action [observable f]
   (.doAction observable f))
 
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; EventStreams
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;
+
+(defn event-stream [f]
+  (js/Bacon.EventStream. f))
 
 (defn to-property
   ([stream]
